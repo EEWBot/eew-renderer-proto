f0916c406e9d80e8107cdea5dea9297a34d6d9c9
igameta <igameta0510@outlook.jp>
2024-10-02 23:48:20 +0900

    [WIP] とりあえずprostにつめてエンコードするとこまで うごかん

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,64 @@ pub mod quake_prefecture {
 }
 
 use prost::Message;
+use quake_prefecture::{CodeArray, Epicenter, QuakePrefectureData};
 
 use serde::Deserialize;
 use serde_json;
 
+#[derive(Deserialize)]
+struct QuakePrefData {
+    rendering_width: u32,
+    quaked_unixtime: u64,
+    epicenter: Option<(i32, i32)>, // (lat_x10, lon_x10)
+    magnitude_x10: Option<u32>,
+    code_table_difinition: String,
 
-impl<'de> Deserialize<'de> for quake_prefecture::QuakePrefectureData {
-    fn deserialize<D>(deserializer: D) -> Result<quake_prefecture::QuakePrefectureData, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let s: String = Deserialize::deserialize(deserializer)?;
-        let qpd: quake_prefecture::QuakePrefectureData = serde_json::from_str(&s).unwrap();
-        Ok(qpd)
-    }
+    one: Option<Vec<u32>>,
+    two: Option<Vec<u32>>,
+    three: Option<Vec<u32>>,
+    four: Option<Vec<u32>>,
+    five_minus: Option<Vec<u32>>,
+    five_plus: Option<Vec<u32>>,
+    six_minus: Option<Vec<u32>>,
+    six_plus: Option<Vec<u32>>,
+    seven: Option<Vec<u32>>,
 }
+
+
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let target = include_str!("20240808075502_0_VXSE53_010000.json");
-    let qpd: quake_prefecture::QuakePrefectureData = serde_json::from_str(target)?;
+    let qpd_json: QuakePrefData = serde_json::from_str(target)?;
+    println!("hoge");
+    
+    let quake_prefecture_data = QuakePrefectureData {
+        rendering_width: qpd_json.rendering_width,
+        quaked_unixtime: qpd_json.quaked_unixtime,
+        magnitude_x10: Some(qpd_json.magnitude_x10.unwrap()),
+        code_table_definition: qpd_json.code_table_difinition,
+        epicenter: Some(Epicenter {
+            lat_x10: qpd_json.epicenter.unwrap().0,
+            lon_x10: qpd_json.epicenter.unwrap().1,
+        }),
+        one: Some(CodeArray { codes: qpd_json.one.unwrap() }),
+        two: Some(CodeArray { codes: qpd_json.two.unwrap() }),
+        three: Some(CodeArray { codes: qpd_json.three.unwrap() }),
+        four: Some(CodeArray { codes: qpd_json.four.unwrap() }),
+        five_minus: Some(CodeArray { codes: qpd_json.five_minus.unwrap() }),
+        five_plus: Some(CodeArray { codes: qpd_json.five_plus.unwrap() }),
+        six_minus: Some(CodeArray { codes: qpd_json.six_minus.unwrap() }),
+        six_plus: Some(CodeArray { codes: qpd_json.six_plus.unwrap() }),
+        seven: Some(CodeArray { codes: qpd_json.seven.unwrap() }),
+    };
+    println!("hoge");
+    
+    let buf: Vec<u8> = quake_prefecture_data.encode_to_vec();
+    wr_b65(&buf)
+}
 
-    let encoded_buf = base65536::encode(&qpd.encode_to_vec(), None);
-    print!("{}", encoded_buf);
+fn wr_b65(buf: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    let encoded = base65536::encode(buf, None);
+    println!("{}", encoded);
     Ok(())
-
 }
